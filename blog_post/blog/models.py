d5fad9aab601a92c9c55f1acb107ba405bd8f2b3
tekla-hub <teklakukhianidze93@gmail.com>
2025-11-09 03:44:58 +0400

    create BlogPost model

--- blog_post/blog/models.py
+++ blog_post/blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
+class BlogPost(models.Model):
+    title = models.CharField(verbose_name="სათაური", max_length=255)
+    text = models.TextField(verbose_name="ტექსტი")
+
+    class Meta:
+        verbose_name = "Blog Post"
+        verbose_name_plural = "Blog Posts"
+
+    def __str__(self):
+        return self.title
+
+
+from django.db import models
+
 # Create your models here.
